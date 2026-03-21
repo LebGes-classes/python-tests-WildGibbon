da2ae26750783582869308534f14e230eb85db66
WildGibbon <chelovekebola@mail.ru>
2026-03-22 01:32:26 +0500

    Revert "u"
    This reverts commit e526234ec172c36df6b56781ac34bb8b0c0d72c3.

--- unit_test/unittest_test.py
+++ unit_test/unittest_test.py
@@ -8,7 +8,6 @@ class TestMyFunctions(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
 
-
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(3, 5), -2)
