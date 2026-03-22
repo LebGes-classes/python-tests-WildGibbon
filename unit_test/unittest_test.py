3adb5bb93f9eeffc0ee7ed9a53570e136a4117cc
WildGibbon <chelovekebola@mail.ru>
2026-03-22 22:44:06 +0500

    Some description corrections

--- unit_test/unittest_test.py
+++ unit_test/unittest_test.py
@@ -5,13 +5,13 @@ import unittest
 class TestMyFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(1, 0), 1)
         self.assertEqual(add(-1, -1), -2)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(3, 5), -2)
-        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(-3, 5), -8)
+        self.assertEqual(subtract(0, 1), -1)
 
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
